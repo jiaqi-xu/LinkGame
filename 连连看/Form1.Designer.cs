c4ca1566ab6a4bdd893bfb7266f022f543a8c343
jiaqi-xu <jiaqi.xu.smile@gmail.com>
2017-06-29 18:36:42 -0400

    LinkGame

---
+++ 连连看/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace 连连看
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.newgamebutton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // newgamebutton
+            // 
+            this.newgamebutton.BackColor = System.Drawing.Color.White;
+            this.newgamebutton.Location = new System.Drawing.Point(193, 12);
+            this.newgamebutton.Name = "newgamebutton";
+            this.newgamebutton.Size = new System.Drawing.Size(75, 23);
+            this.newgamebutton.TabIndex = 0;
+            this.newgamebutton.Text = "新游戏";
+            this.newgamebutton.UseVisualStyleBackColor = false;
+            this.newgamebutton.Click += new System.EventHandler(this.newgamebutton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.jpg");
+            this.imageList1.Images.SetKeyName(1, "2.jpg");
+            this.imageList1.Images.SetKeyName(2, "3.jpg");
+            this.imageList1.Images.SetKeyName(3, "4.jpg");
+            this.imageList1.Images.SetKeyName(4, "5.jpg");
+            this.imageList1.Images.SetKeyName(5, "6.jpg");
+            this.imageList1.Images.SetKeyName(6, "7.jpg");
+            this.imageList1.Images.SetKeyName(7, "8.jpg");
+            this.imageList1.Images.SetKeyName(8, "9.jpg");
+            this.imageList1.Images.SetKeyName(9, "10.jpg");
+            this.imageList1.Images.SetKeyName(10, "11.jpg");
+            this.imageList1.Images.SetKeyName(11, "12.jpg");
+            this.imageList1.Images.SetKeyName(12, "13.jpg");
+            this.imageList1.Images.SetKeyName(13, "14.jpg");
+            this.imageList1.Images.SetKeyName(14, "15.jpg");
+            this.imageList1.Images.SetKeyName(15, "16.jpg");
+            this.imageList1.Images.SetKeyName(16, "17.jpg");
+            this.imageList1.Images.SetKeyName(17, "18.jpg");
+            this.imageList1.Images.SetKeyName(18, "19.jpg");
+            this.imageList1.Images.SetKeyName(19, "20.jpg");
+            this.imageList1.Images.SetKeyName(20, "21.jpg");
+            this.imageList1.Images.SetKeyName(21, "22.jpg");
+            this.imageList1.Images.SetKeyName(22, "23.jpg");
+            this.imageList1.Images.SetKeyName(23, "24.jpg");
+            this.imageList1.Images.SetKeyName(24, "25.jpg");
+            this.imageList1.Images.SetKeyName(25, "26.jpg");
+            this.imageList1.Images.SetKeyName(26, "27.jpg");
+            this.imageList1.Images.SetKeyName(27, "28.jpg");
+            this.imageList1.Images.SetKeyName(28, "29.jpg");
+            this.imageList1.Images.SetKeyName(29, "30.jpg");
+            this.imageList1.Images.SetKeyName(30, "31.jpg");
+            this.imageList1.Images.SetKeyName(31, "32.jpg");
+            this.imageList1.Images.SetKeyName(32, "33.jpg");
+            this.imageList1.Images.SetKeyName(33, "34.jpg");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(287, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "时间：";
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.timelabel.Location = new System.Drawing.Point(359, 16);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(0, 19);
+            this.timelabel.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 653);
+            this.progressBar1.Maximum = 600;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(600, 5);
+            this.progressBar1.Step = 3;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(394, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "图片不见了，点这里！";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(734, 662);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newgamebutton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newgamebutton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 连连看/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 连连看
+{
+    public partial class Form1 : Form
+    {
+        private int X1 = 0;
+        private int X2 = 0;
+        private int Y1 = 0;
+        private int Y2 = 0;
+        private int cishu = 0;//点击鼠标的次数
+        private int[,] arr;
+        private int time = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            arr = new int[22, 22];
+            for (int i = 0; i < 22; i++)
+            {
+                for (int j = 0; j < 22; j++)
+                {
+                    arr[i, j] = -1;
+                }
+            }
+        }
+        private void paint()//画格子
+        {
+            Graphics g = CreateGraphics();
+            g.Clear(Color.White);
+            Pen mypen1 = new Pen(Color.Black);
+            for (int i = 1; i < 22; i++)
+            {
+                g.DrawLine(mypen1, 50, 20 + 30 * i, 650, 20 + 30 * i);
+                g.DrawLine(mypen1, 20 + 30 * i, 50, 20 + 30 * i, 650);
+                if(i<21)
+                {
+                    for (int j = 1; j < 21; j++)
+                    {
+                        if (arr[i, j] != -1)
+                            g.DrawImage(imageList1.Images[arr[i, j]], 21 + 30 * i, 21 + 30 * j, 29, 29);
+                    }
+                }
+            }
+        }
+        private void paint1()//画空白
+        {
+            Graphics g = CreateGraphics();
+            SolidBrush mybrush1 = new SolidBrush(Color.White);
+            for (int i = 0; i < 22; i++)
+            {
+                for (int j = 0; j < 22; j++)
+                {
+                    if (arr[i, j] == -1)
+                    {
+                        g.FillRectangle(mybrush1, 21 + 30 * i, 21 + 30 * j, 29, 29);
+                    }
+                }
+
+            }
+        }
+        private void paigezi()//对格子进行排序
+        {
+            Random ran = new Random();
+            for (int i = 0; i < 200; i++)
+            {
+                int x1 = ran.Next(1, 21);
+                int y1 = ran.Next(1, 21);
+                int x2 = ran.Next(1, 21);
+                int y2 = ran.Next(1, 21);
+                int z = ran.Next(0, 34);
+                if (arr[x1, y1] == -1 && arr[x2, y2] == -1&&(x1!=x2||y1!=y2))
+                {
+                    arr[x1, y1] = z;
+                    arr[x2, y2] = z;
+                }
+                else
+                    i--;
+            }
+        }
+        private void newgamebutton_Click(object sender, EventArgs e)
+        {
+            time = 0;
+            progressBar1.Value = 0;
+            timer1.Enabled = true;
+            for (int i = 1; i < 21; i++)
+            {
+                for (int j = 1; j < 21; j++)
+                {
+                    arr[i, j] = -1;
+                }
+            }
+            paigezi();
+            paint();
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.X >= 50 && e.X <= 650 && e.Y >= 50 && e.Y <= 650)
+            {
+                cishu++;
+                if (cishu == 1)
+                {
+                    X1 = (e.X - 20) / 30;
+                    Y1 = (e.Y - 20) / 30;
+                    xiaochu();
+                }
+                else
+                {
+                    X2 = (e.X - 20) / 30;
+                    Y2 = (e.Y - 20) / 30;
+                    cishu = 0;
+                    xiaochu();
+                }
+            }
+        }
+        private void xiaochu()
+        {
+            //从左到右
+            for (int i = 0; i < 22; i++)
+            {
+                if (f1(i, X1, Y1) && f1(i, X2, Y2) && f4(i, Y1, Y2) && (X1 != X2 || Y1 != Y2) && arr[X1, Y1] == arr[X2, Y2] && arr[X1, Y1]!=-1)//三条路都通
+                {
+                    arr[X1, Y1] = -1;
+                    arr[X2, Y2] = -1;
+                    Graphics g = CreateGraphics();
+                    Pen mypen1 = new Pen(Color.Black);
+                    SolidBrush mybrush1 = new SolidBrush(Color.White);
+                    g.FillRectangle(mybrush1, 21 + 30 * X1, 21 + 30 * Y1, 29, 29);
+                    g.FillRectangle(mybrush1, 21 + 30 * X2, 21 + 30 * Y2, 29, 29);
+                    g.DrawLine(mypen1, 35 + 30 * X1, 35 + 30 * Y1, 35 + 30 * i, 35 + 30 * Y1);
+                    g.DrawLine(mypen1, 35 + 30 * X2, 35 + 30 * Y2, 35 + 30 * i, 35 + 30 * Y2);
+                    g.DrawLine(mypen1, 35 + 30 * i, 35 + 30 * Y1, 35 + 30 * i, 35 + 30 * Y2);
+                    progressBar1.Value += 3;
+                    if (shibai() == false)
+                        MessageBox.Show("没有可以连的了，重新再来吧！");
+                }
+            }
+            //从上到下
+            for (int i = 0; i < 22; i++)
+            {
+                if (f2(i, Y1, X1) && f2(i, Y2, X2) && f3(i, X1, X2) && (X1 != X2 || Y1 != Y2) && arr[X1, Y1] == arr[X2, Y2] && arr[X1, Y1] != -1)//三条路都通
+                {
+                    arr[X1, Y1] = -1;
+                    arr[X2, Y2] = -1;
+                    Graphics g = CreateGraphics();
+                    Pen mypen1 = new Pen(Color.Black);
+                    SolidBrush mybrush1 = new SolidBrush(Color.White);
+                    g.FillRectangle(mybrush1, 21 + 30 * X1, 21 + 30 * Y1, 29, 29);
+                    g.FillRectangle(mybrush1, 21 + 30 * X2, 21 + 30 * Y2, 29, 29);
+                    g.DrawLine(mypen1, 35 + 30 * X1, 35 + 30 * Y1, 35 + 30 *X1, 35 + 30 * i);
+                    g.DrawLine(mypen1, 35 + 30 * X2, 35 + 30 * Y2, 35 + 30 *X2, 35 + 30 * i);
+                    g.DrawLine(mypen1, 35 + 30 * X1, 35 + 30 * i, 35 + 30 *X2, 35 + 30 * i);
+                    progressBar1.Value += 3;
+                    if (shibai() == false)
+                        MessageBox.Show("没有可以连的了，重新再来吧！");
+                }
+            }
+        }
+        private bool f1(int i, int j, int k)//判断行路是否通
+        {
+            for (; i < j; )
+            {
+                if (arr[i, k] != -1)//该格不同
+                    return false;
+                else
+                    i++;
+            }
+            for (; i > j; )
+            {
+                if (arr[i, k] != -1)//该格不同
+                    return false;
+                else
+                    i--;
+            }
+            return true;
+        }
+        private bool f2(int i, int j, int k)//判断列路是否通
+        {
+            for (; i < j; )
+            {
+                if (arr[k, i] != -1)//该格不同
+                    return false;
+                else
+                    i++;
+            }
+            for (; i > j; )
+            {
+                if (arr[k,i] != -1)//该格不同
+                    return false;
+                else
+                    i--;
+            }
+            return true;
+        }
+        private bool f3(int i, int j,int k)//判断某行路是否通
+        {
+            for (; k < j-1; )
+            {
+                if (arr[k, i] != -1)//该格不同
+                    return false;
+                else
+                    k++;
+            }
+            for (; k > j+1; )
+            {
+                if (arr[k, i] != -1)//该格不同
+                    return false;
+                else
+                    k--;
+            }
+            return true;
+        }
+        private bool f4(int i, int j, int k)//判断某列路是否通
+        {
+            for (; j < k-1; )
+            {
+                if (arr[i, j] != -1)//该格不同
+                    return false;
+                else
+                    j++;
+            }
+            for (; j > k+1; )
+            {
+                if (arr[i, j] != -1)//该格不同
+                    return false;
+                else
+                    j--;
+            }
+            return true;
+        }
+        private bool shibai()
+        {
+            int n = 0;//消除的个数
+            for (int i = 1; i < 21; i++)
+            {
+                for (int j = 1; j < 21; j++)
+                {
+                    if (arr[i, j] == -1)
+                        n++;
+                }
+            }
+            if (n == 400)
+            {
+                MessageBox.Show("游戏胜利，请继续保持");
+                return true;
+            }
+            else
+            {
+                for (int x1 = 1; x1 < 21; x1++)
+                {
+                    for (int y1 = 1; y1 < 21; y1++)
+                    {
+                        for (int x2 = 1; x2 < 21; x2++)
+                        {
+                            for (int y2 = 1; y2 < 21; y2++)
+                            {
+                                for (int i = 0; i < 22; i++)
+                                {
+
+                                    if (f1(i, x1, y1) && f1(i, x2, y2) && f4(i, y1, y2) && (x1 != x2 || y1 != y2) && arr[x1, y1] == arr[x2, y2] && arr[x1, y1] != -1)//三条路都通
+                                    {
+                                        return true;
+                                    }
+                                }
+                                for (int i = 0; i < 22; i++)
+                                {
+                                    if (f2(i, y1, x1) && f2(i, y2, x2) && f3(i, x1, x2) && (x1 != x2 || y1 != y2) && arr[x1, y1] == arr[x2, y2] && arr[x1, y1] != -1)//三条路都通
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            paint1();
+            time++;
+            timelabel.Text = time.ToString() + "s";
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            paint();
+        }
+    }
+}
